d51ee6fc782183b7f9fa63150fe6bf2f7ed513d1
pyy2114 <pyy2114@naver.com>
2025-03-17 21:04:13 +0900

    feat:#1 ExceptionHandler 추가

--- src/main/kotlin/com/project/community/controller/BoardController.kt
+++ src/main/kotlin/com/project/community/controller/BoardController.kt
@@ -3,6 +3,10 @@ package com.project.community.controller
 import com.project.community.dto.BoardDetailsDTO
 import com.project.community.dto.BoardListDTO
 import com.project.community.service.BoardService
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -24,5 +28,11 @@ class BoardController(
     @GetMapping("/{boardId}")
     fun getBoardDetails(@PathVariable boardId: Long): BoardDetailsDTO{
         return boardService.getBoardDetails(boardId)
+    }
+
+    //IllegalStateException 발생 시 400으로 반환
+    @ExceptionHandler(IllegalStateException::class)
+    fun handlerIllegalStateException(ex: IllegalStateException): ResponseEntity<String>{
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.message)
     }
 }
